b044f382ba7cfd67f8fcf027f63e4047a67cfc05
vishwanath Malakar <vishwanathmalakar@gmail.com>
2018-12-08 18:24:09 +0530

    first commit

---
+++ src/test/java/com/Interview/RestAssured/AppTest.java
@@ -0,0 +1,13 @@
+package com.Interview.RestAssured;
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+	@Test
+	public void Hello()
+	{
+		System.out.println( "Test : Hello World!" );
+		
+	}
+
+}
